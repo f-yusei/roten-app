1d0d644276b37ebcf1e2087dff9e40e56cc0c1ad
f-yusei <fkys2932@gmail.com>
2023-10-05 11:55:33 +0900

    "feat: トッピング内容のUIの雛形を作成"

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -2,12 +2,14 @@ import { FC } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import NotFound from '../NotFound';
 import { ReceptionPage } from '../components/pages/Reception';
+import { ToppingPage } from '../components/pages/Topping';
 
 export const Router: FC = () => {
   return (
     <Routes>
       <Route path={'/'} element={<div>トップページ</div>} />
       <Route path={'/reception'} element={<ReceptionPage />} />
+      <Route path={'/topping'} element={<ToppingPage />} />
       <Route path={'*'} element={<NotFound />} />
     </Routes>
   );
