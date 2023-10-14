95ddec87cb263c2100c26871e6187fc41d474970
f-yusei <fkys2932@gmail.com>
2023-10-14 14:51:18 +0900

    "feat: バックスペースアイコンを追加"

---
+++ frontend/src/common/BackspaceIcon.tsx
@@ -0,0 +1,18 @@
+import { createIcon } from '@chakra-ui/react';
+
+export const BackspaceIcon = createIcon({
+  displayName: 'BackspaceIcon',
+  viewBox: '0 0 44.18 44.18',
+  path: (
+    <>
+      <path
+        fill="currentColor"
+        d="M10.625 5.09 0 22.09l10.625 17H44.18v-34H10.625zm31.555 32H11.734l-9.375-15 9.375-15H42.18v30z "
+      />
+      <path
+        fill="currentColor"
+        d="m18.887 30.797 7.293-7.293 7.293 7.293 1.414-1.414-7.293-7.293 7.293-7.293-1.414-1.414-7.293 7.293-7.293-7.293-1.414 1.414 7.293 7.293-7.293 7.293z"
+      />
+    </>
+  ),
+});
