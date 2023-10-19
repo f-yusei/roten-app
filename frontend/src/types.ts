9cdca05ef6a6e0d8931d258a03b87051a3593d2e
f-yusei <fkys2932@gmail.com>
2023-10-19 14:20:47 +0900

    "feat: オーダーの型を定義"

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,5 +1,15 @@
-export type OrderInformation = {
+export type ValidOrder = {
   orderNumber: number;
   menu: string;
-  topping: string;
+  arranges: string[];
+  orderState: 'waiting' | 'available' | 'finished';
+  created_at?: string;
+};
+
+export type Order = {
+  orderNumber: number;
+  menu: string;
+  arranges: string[];
+  orderState?: 'waiting' | 'available' | 'finished';
+  created_at?: string;
 };
