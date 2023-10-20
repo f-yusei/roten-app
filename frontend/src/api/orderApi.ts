d07b09ada1b3ffac933fa8864e7be87976b62d77
f-yusei <fkys2932@gmail.com>
2023-10-20 11:08:52 +0900

    "feat: 注文API一式を実装"

---
+++ frontend/src/api/orderApi.ts
@@ -0,0 +1,10 @@
+import apiClient from './axiosClient';
+
+const orderApi = {
+  getAll: () => apiClient.get('/orders'),
+  store: (params: any) => apiClient.post('/orders', params),
+  update: (params: any) => apiClient.put(`/orders/${params.id}`, params),
+  delete: (id: number) => apiClient.delete(`/orders/${id}`),
+};
+
+export default orderApi;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <ChakraProvider>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>,
