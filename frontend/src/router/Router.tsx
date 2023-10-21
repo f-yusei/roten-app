b37a087ee13ad09a9af5ebf543231921c127018a
Hirakuhito <takuto.yoshino1210@icloud.com>
2023-10-21 14:20:34 +0900

    "feat: TopPageの実装"

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -4,11 +4,12 @@ import NotFound from '../NotFound';
 import { ReceptionPage } from '../components/pages/Reception';
 import { ToppingPage } from '../components/pages/Topping';
 import {DeliveryPage} from '../components/pages/Delivery'
+import { TopPage } from '../components/pages/TopPage';
 
 export const Router: FC = () => {
   return (
     <Routes>
-      <Route path={'/'} element={<div>トップページ</div>} />
+      <Route path={'/'} element={<TopPage/>} />
       <Route path={'/reception'} element={<ReceptionPage />} />
       <Route path={'/topping'} element={<ToppingPage />} />
       <Route path={'/delivery'} element={<DeliveryPage/>} />
