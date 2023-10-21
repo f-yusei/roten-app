129d3cb0eb8ee9f44ca794c55f3bdd4e14cb171f
akito-0520 <n.akito.h17@icloud.com>
2023-10-21 21:03:35 +0900

    "fix: UIの微調整"

--- frontend/src/components/features/reception/presenter/Calculator.tsx
+++ frontend/src/components/features/reception/presenter/Calculator.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, IconButton, Input } from '@chakra-ui/react';
+import { Button, Grid, GridItem, IconButton, Input } from '@chakra-ui/react';
 import { BackspaceIcon } from '../../../../common/BackspaceIcon';
 import React from 'react';
 import OrderConfirmationModal from './OrderConfirmationModal';
@@ -29,46 +29,172 @@ const Calculator = () => {
   };
 
   return (
-    <Box>
-      <Input isDisabled value={value} />
-      <Box>
-        <Button onClick={useTicket}>100円券</Button>
-        <Button onClick={handleClear}>Clear</Button>
-        <IconButton
-          aria-label="Backspace"
-          icon={<BackspaceIcon />}
-          onClick={handleBackspace}
-        ></IconButton>
-        <Button name="7" onClick={handleClick}>
-          7
-        </Button>
-        <Button name="8" onClick={handleClick}>
-          8
-        </Button>
-        <Button name="9" onClick={handleClick}>
-          9
-        </Button>
-        <Button name="4" onClick={handleClick}>
-          4
-        </Button>
-        <Button name="5" onClick={handleClick}>
-          5
-        </Button>
-        <Button name="6" onClick={handleClick}>
-          6
-        </Button>
-        <Button name="1" onClick={handleClick}>
-          1
-        </Button>
-        <Button name="2" onClick={handleClick}>
-          2
-        </Button>
-        <Button name="3" onClick={handleClick}>
-          3
-        </Button>
-        <OrderConfirmationModal />
-      </Box>
-    </Box>
+    <>
+      <Grid
+        templateRows="repeat(2, 1fr)"
+        templateColumns="repeat(3, 1fr)"
+      >
+        <GridItem rowSpan={1} colSpan={3}>
+          <Input isDisabled value={value}
+            fontSize="2.4rem"
+            width="42vw"
+            height="12vh"
+          />
+        </GridItem>
+        <GridItem rowSpan={1} colSpan={1}>
+          <Button onClick={useTicket}
+            fontSize="2.4rem"
+            width="12vw"
+            height="12vh"
+            mr={9}
+          >
+            100円券
+          </Button>
+        </GridItem>
+        <GridItem rowSpan={1} colSpan={1}>
+          <Button onClick={handleClear}
+            fontSize="2.4rem"
+            width="12vw"
+            height="12vh"
+            m={1}
+          >
+            Clear
+          </Button>
+        </GridItem>
+        <GridItem rowSpan={1} colSpan={1} >
+          <IconButton
+            fontSize="2.4rem"
+            width="12vw"
+            height="12vh"
+            m={1}
+            aria-label="Backspace"
+            icon={<BackspaceIcon />}
+            onClick={handleBackspace}
+          ></IconButton>
+        </GridItem>
+      </Grid>
+      <Grid templateRows="repeat(4, 1fr)"
+        templateColumns="repeat(3, 1fr)">
+        <GridItem rowSpan={1} colSpan={1}>
+          <Button name="1" onClick={handleClick}
+            fontSize="2.4rem"
+            width="12vw"
+            height="12vh"
+            alignItems="center"
+            m={1}
+          >
+            1
+          </Button>
+        </GridItem>
+        <GridItem rowSpan={1} colSpan={1}>
+          <Button name="2" onClick={handleClick}
+            fontSize="2.4rem"
+            width="12vw"
+            height="12vh"
+            alignItems="center"
+            m={1}
+          >
+            2
+          </Button>
+        </GridItem>
+        <GridItem rowSpan={1} colSpan={1}>
+          <Button name="3" onClick={handleClick}
+            fontSize="2.4rem"
+            width="12vw"
+            height="12vh"
+            alignItems="center"
+            m={1}
+          >
+            3
+          </Button>
+        </GridItem>
+        <GridItem rowSpan={1} colSpan={1}>
+          <Button name="4" onClick={handleClick}
+            fontSize="2.4rem"
+            width="12vw"
+            height="12vh"
+            m={1}
+          >
+            4
+          </Button>
+        </GridItem>
+        <GridItem rowSpan={1} colSpan={1}>
+          <Button name="5" onClick={handleClick}
+            fontSize="2.4rem"
+            width="12vw"
+            height="12vh"
+            m={1}
+          >
+            5
+          </Button>
+        </GridItem>
+        <GridItem rowSpan={1} colSpan={1}>
+          <Button name="6" onClick={handleClick}
+            fontSize="2.4rem"
+            width="12vw"
+            height="12vh"
+            m={1}
+          >
+            6
+          </Button>
+        </GridItem>
+        <GridItem rowSpan={1} colSpan={1}>
+          <Button name="7" onClick={handleClick}
+            fontSize="2.4rem"
+            width="12vw"
+            height="12vh"
+            m={1}
+          >
+            7
+          </Button>
+        </GridItem>
+        <GridItem rowSpan={1} colSpan={1}>
+          <Button name="8" onClick={handleClick}
+            fontSize="2.4rem"
+            width="12vw"
+            height="12vh"
+            m={1}
+          >
+            8
+          </Button>
+        </GridItem>
+        <GridItem rowSpan={1} colSpan={1}>
+          <Button name="9" onClick={handleClick}
+            fontSize="2.4rem"
+            width="12vw"
+            height="12vh"
+            m={1}
+          >
+            9
+          </Button>
+        </GridItem >
+        <GridItem rowSpan={1} colSpan={1}>
+          <Button name="0" onClick={handleClick}
+            fontSize="2.4rem"
+            width="12vw"
+            height="12vh"
+            m={1}
+          >
+            0
+          </Button>
+        </GridItem>
+        <GridItem rowSpan={1} colSpan={1}>
+          <Button name="00" onClick={handleClick}
+            fontSize="2.4rem"
+            width="12vw"
+            height="12vh"
+            m={1}
+          >
+            00
+          </Button>
+        </GridItem>
+      </Grid>
+      <Grid>
+        <GridItem rowSpan={1} colSpan={1}>
+          <OrderConfirmationModal />
+        </GridItem>
+      </Grid >
+    </>
   );
 };
 
