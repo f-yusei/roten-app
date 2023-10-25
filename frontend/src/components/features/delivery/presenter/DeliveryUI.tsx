72cf4b4a2b1373d39ddfbf4d5e356ef445390f5e
Hirakuhito <takuto.yoshino1210@icloud.com>
2023-10-26 04:42:13 +0900

    "feat:DeliveryUIのUI変更"

--- frontend/src/components/features/delivery/presenter/DeliveryUI.tsx
+++ frontend/src/components/features/delivery/presenter/DeliveryUI.tsx
@@ -13,6 +13,7 @@ import { useEffect, useState } from 'react';
 import orderApi from '../../../../api/orderApi';
 import { useGetAllOrder } from '../../../../api/hooks';
 import { v4 } from 'uuid';
+import { ClientRequest } from 'http';
 
 const DeliveryUI = () => {
   const { orders, isLoading } = useGetAllOrder();
@@ -37,7 +38,7 @@ const DeliveryUI = () => {
           arranges: {
             kind: 'sauce',
             sauce: true,
-            mayo: true,
+            mayo: false,
             katsuo: true,
             aosa: true,
           },
@@ -122,59 +123,108 @@ const DeliveryUI = () => {
 
   return (
     <HStack>
-      <Grid templateColumns="repeat(2, 1fr)">
-        {availableOrders.slice(0, 6).map((order) => (
-          <GridItem key={v4()}>
-            <Button
-              w="24vw"
-              h="24vh"
-              bgColor="gray.100"
-              margin={2}
-              onClick={() => handleClick(order)}
-            >
-              {order.woodenNumber}
-            </Button>
-            {
-              //TODO: ここで時間を表示する
-              /* <Box>
+      <Box w="53vw" h="98vh" bgColor={'gray.20'} border="2px"  borderRadius="10px" >
+        <Grid templateColumns="repeat(2, 1fr)">
+          {availableOrders.slice(0, 6).map((order) => (
+            <GridItem key={v4()}>
+              <Button
+                w="24vw"
+                h="24vh"
+                bgColor="gray.100"
+                margin={2}
+                fontSize={120}
+                fontWeight={'bold'}
+                border="2px"
+                borderColor={'#bfbfbf'}
+                onClick={() => handleClick(order)}
+              >
+                {order.woodenNumber}
+              </Button>
+              {
+                //TODO: ここで時間を表示する
+                /* <Box>
               {order.orderStateLogs.readiedAt
                 ? calculateSec(order) + '秒'
                 : 'lording...'}
             </Box> */
-            }
-          </GridItem>
-        ))}
-      </Grid>
+              }
+            </GridItem>
+          ))}
+        </Grid>
+      </Box>
       <Box>
         <Box>
-          <Card h={'44vh'}>
+          <Card h={'44vh'} border="2px">
             <CardBody>
-              <Box height="25vh">
-                <Box>
+              <Box height="25vh" >
+                <Box
+                  w="5vw"
+                  bgColor={'#ffeb99'}
+                  textAlign={'center'}
+                  ml={'auto'}
+                  p={1}
+                  fontSize={15}
+                  fontWeight={'bold'}
+                  border="2px"
+                  borderRadius="10px"
+                  borderColor={'#fdd630'}
+                >
                   {currentSelectOrder?.woodenNumber
                     ? currentSelectOrder.woodenNumber
                     : 'lording...'}
                 </Box>
-                {currentSelectOrder?.menus.map((menu) => (
-                  <Box key={v4()}>
-                    <Box>{menu.name}</Box>
-                    <Box>トッピング</Box>
-                    {Object.entries(menu.arranges).map(([topping, value]) => {
-                      if (!value) {
-                        return <Box key={topping}>-no {topping}</Box>;
-                      }
-                      return null;
-                    })}
-                  </Box>
-                ))}
+                <Box
+                  m={'auto'}
+                  p={3}
+                  w="20vw"
+                  h="24vh"
+                  bgColor={'gray.100'}
+                  overflow={'scroll'}
+                  borderRadius="10px"
+                  border="2px"
+                  borderColor={'#bfbfbf'}
+                >
+                  {currentSelectOrder?.menus.map((menu) => (
+                    <Box key={v4()}>
+                      {menu.name == 'ソース' ||
+                      menu.name == 'ソース前売り' ||
+                      menu.name == 'ソース（セット）前売り' ? (
+                        <Box
+                          fontSize={20}
+                          color={'#8c5403'}
+                          fontWeight={'bold'}
+                        >
+                          {menu.name}
+                        </Box>
+                      ) : (
+                        <Box
+                          fontSize={20}
+                          color={'#ffb3d9'}
+                          fontWeight={'bold'}
+                        >
+                          {menu.name}
+                        </Box>
+                      )}
+
+                      {Object.entries(menu.arranges).map(([topping, value]) => {
+                        if (!value) {
+                          return <Box key={topping}>-no {topping}</Box>;
+                        }
+                        return null;
+                      })}
+                    </Box>
+                  ))}
+                </Box>
               </Box>
               <div className="oprButton">
                 <HStack spacing="50px">
                   <Button
                     onClick={discardOrder}
                     height="8vh"
-                    width="10vw"
-                    m={5}
+                    width="14vw"
+                    fontWeight={'bold'}
+                    mt={12}
+                    fontSize={30}
                     bgColor="red.500"
                   >
                     廃棄
@@ -182,9 +232,11 @@ const DeliveryUI = () => {
                   <Button
                     onClick={finishOrderDelivery}
                     height="8vh"
-                    width="15vw"
-                    m={5}
+                    width="19vw"
+                    mt={12}
                     bgColor="green.300"
+                    fontSize={30}
+                    fontWeight={'bold'}
                   >
                     受け渡し
                   </Button>
@@ -193,7 +245,7 @@ const DeliveryUI = () => {
             </CardBody>
           </Card>
         </Box>
-        <Box h={'44vh'}>
+        <Box h={'55vh'} border="2px"  borderRadius="10px">
           <PureCarousel
             cardInformation={[
               {
